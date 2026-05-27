78d7ff10cd3095b78ef50c4e52cc5147f3039f4d
Li Huang <lihuang.dmft@gmail.com>
2026-05-27 18:25:49 +0800

    fix: update test_crate_metadata version assertion to 0.0.9

--- src/lib.rs
+++ src/lib.rs
@@ -172,8 +172,8 @@ mod tests {
         );
         assert_eq!(
             env!("CARGO_PKG_VERSION"),
-            "0.0.8",
-            "Cargo.toml [package] version must be '0.0.8'"
+            "0.0.9",
+            "Cargo.toml [package] version must be '0.0.9'"
         );
     }
 
