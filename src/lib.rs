d9de27628f8143e937f00e40aa398b6b2cbcb330
Li Huang <lihuang.dmft@gmail.com>
2026-06-05 22:24:06 +0800

    release: Sync version assertion in lib.rs to 0.0.29

--- src/lib.rs
+++ src/lib.rs
@@ -172,8 +172,8 @@ mod tests {
         );
         assert_eq!(
             env!("CARGO_PKG_VERSION"),
-            "0.0.28",
-            "Cargo.toml [package] version must be '0.0.28'"
+            "0.0.29",
+            "Cargo.toml [package] version must be '0.0.29'"
         );
     }
 
